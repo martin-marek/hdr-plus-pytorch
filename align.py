a7dbcf523155d1fca736fdc9de0b3e2544911713
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2023-05-26 11:31:35 +0100

    use first class dimensions

--- align.py
+++ align.py
@@ -2,94 +2,10 @@ import torch
 import torchvision
 import torch.nn.functional as F
 import torchvision.transforms.functional as TF
+from functorch.dim import dims
 
 from torch import Tensor
-from typing import Tuple, List, Optional
-
-
-@torch.jit.script
-def n_tiles(layer_h: int, layer_w: int,
-            tile_h: int, tile_w: int
-           ) -> Tuple[int, int]:
-    """
-    Compute the desired number of tiles in a layer,
-    such that they overlap.
-    """
-    n_tiles_y = layer_h // (tile_h // 2) - 1
-    n_tiles_x = layer_w // (tile_w // 2) - 1
-    return n_tiles_x, n_tiles_y
-
-
-@torch.jit.script
-def tile_indices(layer_w: int, layer_h: int,
-                 tile_w: int, tile_h: int,
-                 n_tiles_x: int, n_tiles_y: int,
-                 device: torch.device
-                ) -> Tuple[Tensor, Tensor]:
-    """
-    Computes the indices of tiles along a layer.
-    """
-    # compute x indices
-    x_min = torch.linspace(0, layer_w-tile_w, n_tiles_x, dtype=torch.int16, device=device) # [n_tiles_x]
-    dx = torch.arange(0, tile_w, device=device) # [tile_w]
-    x = x_min[:, None] + dx[None, :] # [n_tiles_x, tile_w]
-    x = x[None, :, None, :].repeat(n_tiles_y, 1, tile_h, 1) # [n_tiles_y, n_tiles_x, tile_h, tile_w]
-    
-    # compute y indices
-    y_min = torch.linspace(0, layer_h-tile_h, n_tiles_y, dtype=torch.int16, device=device) # [n_tiles_y]
-    dy = torch.arange(0, tile_h, device=device) # [tile_h]
-    y = y_min[:, None] + dy[None, :] # [n_tiles_y, tile_h]
-    y = y[:, None, :, None].repeat(1, n_tiles_x, 1, tile_w) # [n_tiles_y, n_tiles_x, tile_h, tile_w]
-    
-    return x, y # (2 x [n_tiles_y, n_tiles_x, tile_h, tile_w])
-
-
-@torch.jit.script
-def shift_indices(x: Tensor, y: Tensor,
-                  search_dist_min: int,
-                  search_dist_max: int
-                 ) -> Tuple[Tensor, Tensor]:
-    """
-    Given a tensor of tile indices, shift these indices by
-    [search_dist_min, search_dist_max] in both x- and y-directions.
-    This creates (search_dist_max - search_dist_min + 1)^2
-    tile "displacements". All the displacements are returned
-    along the first (batch) dimension. To recover the displacement
-    from the batch dimension, use the following conversion:
-    >>> dy = idx // n_pos + search_dist_min
-    >>> dx = idx % n_pos + search_dist_min
-    """
-    # create a tensor of displacements
-    device = x.device
-    ds = torch.arange(search_dist_min, search_dist_max+1, device=device)
-    n_pos = len(ds)
-    
-    # compute x indices
-    x = x[:, :, :, :, None, None] + ds[None, None, None, None, None, :] # [n_tiles_y, n_tiles_x, tile_h, tile_w, 1, n_pos]
-    x = x.repeat(1, 1, 1, 1, n_pos, 1) # [n_tiles_y, n_tiles_x, tile_h, tile_w, n_pos, n_pos]
-    x = x.flatten(-2) # [n_tiles_y, n_tiles_x, tile_h, tile_w, n_pos*n_pos]
-    x = x.permute(4, 0, 1, 2, 3) # [n_pos*n_pos, n_tiles_y, n_tiles_x, tile_h, tile_w]
-    
-    # compute y indices
-    y = y[:, :, :, :, None, None] + ds[None, None, None, None, :, None] # [n_tiles_y, n_tiles_x, tile_h, tile_w, n_pos, 1]
-    y = y.repeat(1, 1, 1, 1, 1, n_pos) # [n_tiles_y, n_tiles_x, tile_h, tile_w, n_pos, n_pos]
-    y = y.flatten(-2) # [n_tiles_y, n_tiles_x, tile_h, tile_w, n_pos*n_pos]
-    y = y.permute(4, 0, 1, 2, 3) # [n_pos*n_pos, n_tiles_y, n_tiles_x, tile_h, tile_w]
-    
-    return x, y # (2 x [n_pos*n_pos, n_tiles_y, n_tiles_x, tile_h, tile_w])
-
-
-@torch.jit.script
-def clamp(x: Tensor, y: Tensor,
-          layer_w: int, layer_h: int
-         ) -> Tuple[Tensor, Tensor]:
-    """
-    Clamp indices to layer shape.
-    """
-    # use inplace operations to reduce momory usage
-    x.clamp_(0, layer_w-1)
-    y.clamp_(0, layer_h-1)
-    return x, y
+from typing import List
 
 
 @torch.jit.script
@@ -128,12 +44,11 @@ def build_pyramid(image: Tensor,
     return pyramid
 
 
-@torch.jit.script
 def align_layers(ref_layer: Tensor,
                  comp_layer: Tensor,
-                 tile_shape: List[int],
-                 search_region: List[int],
                  prev_alignment: Tensor,
+                 tile_size: int,
+                 search_dist: int,
                  downscale_factor: int = 1
                 ) -> Tensor:
     """
@@ -145,57 +60,50 @@ def align_layers(ref_layer: Tensor,
         downscale_factor: scaling factor between the previous layer and current layer, only required if `prev_alignment` is not zeros
     """
     device = ref_layer.device
-    # compute dimensions of layer and tiles
-    _, layer_h, layer_w = ref_layer.shape
-    tile_w, tile_h = tile_shape
-    n_tiles_x, n_tiles_y = n_tiles(layer_h, layer_w, tile_h, tile_w)
-    search_dist_min, search_dist_max = search_region
-    n_pos = search_dist_max - search_dist_min + 1
 
-    # gather tiles from the reference layer
-    x, y = tile_indices(layer_w, layer_h, tile_w, tile_h, n_tiles_x, n_tiles_y, device) # [n_tiles_y, n_tiles_x, tile_h, tile_w]
-    ref_tiles = ref_layer[:, y, x] # [1, n_tiles_y, n_tiles_x, tile_h, tile_w]
+    # compute number of tiles in a layer such that they overlap
+    n_channels, layer_height, layer_width = ref_layer.shape
+    n_tiles_y = layer_height // (tile_size // 2) - 1
+    n_tiles_x = layer_width  // (tile_size // 2) - 1
+    
+    # upscale previous alignment
+    prev_alignment = upscale_previous_alignment(prev_alignment, downscale_factor, n_tiles_x, n_tiles_y)
 
-    # compute coordinates for comparison tiles
-    x, y = tile_indices(layer_w, layer_h, tile_w, tile_h, n_tiles_x, n_tiles_y, device) # [n_tiles_y, n_tiles_x, tile_h, tile_w]
-    prev_alignment = upscale_previous_alignment(prev_alignment, downscale_factor, n_tiles_x, n_tiles_y) # [2, n_tiles_y, n_tiles_x]
-    x += prev_alignment[0, :, :, None, None]
-    y += prev_alignment[1, :, :, None, None]
-    x, y = shift_indices(x, y, search_dist_min, search_dist_max) # [n_pos*n_pos, n_tiles_y, n_tiles_x, tile_h, tile_w]
-    
-    # check if each comparison tile is fully within the layer dimensions
-    tile_is_inside_layer = torch.ones([n_pos*n_pos, n_tiles_y, n_tiles_x], dtype=torch.bool, device=device)
-    tile_is_inside_layer &= x[:, :, :,  0,  0] >= 0
-    tile_is_inside_layer &= x[:, :, :,  0, -1] < layer_w
-    tile_is_inside_layer &= y[:, :, :,  0,  0] >= 0
-    tile_is_inside_layer &= y[:, :, :, -1,  0] < layer_h
-    
-    # gather tiles from the comparison layer
-    x, y = clamp(x, y, layer_w, layer_h) # [n_pos*n_pos, n_tiles_y, n_tiles_x, tile_h, tile_w]
-    comp_tiles = comp_layer[0, y, x] # [n_pos*n_pos, n_tiles_y, n_tiles_x, tile_h, tile_w]
+    # get reference image tiles (no shift)
+    channel, tile_idx_y, tile_idx_x, tile_h, tile_w = dims(sizes=[None, n_tiles_y, n_tiles_x, tile_size, tile_size])
+    x_min = torch.linspace(0, layer_width-tile_size,  n_tiles_x, dtype=torch.int16, device=device)[tile_idx_x]
+    y_min = torch.linspace(0, layer_height-tile_size, n_tiles_y, dtype=torch.int16, device=device)[tile_idx_y]
+    x = x_min + tile_w
+    y = y_min + tile_h
+    ref_tiles = ref_layer[channel, y, x]
 
-    # compute the difference between the comparison and reference tiles
-    diff = comp_tiles - ref_tiles # [n_pos*n_pos, n_tiles_y, n_tiles_x, tile_h, tile_w]
-    diff = diff.abs().sum(dim=[-2, -1]) # [n_pos*n_pos, n_tiles_y, n_tiles_x]
-    
+    # get comparison image tiles (shifted)
+    shift_x, shift_y = dims(sizes=[1+2*search_dist, 1+2*search_dist])
+    x = (x + prev_alignment[0, tile_idx_y, tile_idx_x] + (shift_x - search_dist)).clip(0, layer_width-1)
+    y = (y + prev_alignment[1, tile_idx_y, tile_idx_x] + (shift_y - search_dist)).clip(0, layer_height-1)
+    comp_tiles = comp_layer[channel, y, x]
+
+    # compute the difference between the reference and comparison tiles
+    diff = (ref_tiles - comp_tiles).abs().sum([channel, tile_w, tile_h])
+    diff = diff.order(tile_idx_y, tile_idx_x, (shift_y, shift_x))
+
     # set the difference value for tiles outside of the frame to infinity
+    tile_is_inside_layer = ((x<0)^(x>=layer_width)).sum(tile_w) + ((y<0)^(y>=layer_height)).sum(tile_h) == 0
+    tile_is_inside_layer = tile_is_inside_layer.order(tile_idx_y, tile_idx_x, (shift_y, shift_x))
     diff[~tile_is_inside_layer] = float('inf')
 
     # find which shift (dx, dy) between the reference and comparison tiles yields the lowest loss
-    # - 'torch.div' is used here instead of '//' to avoid a 'UserWarning' in PyTorch
-    #   however, 'torch.div' and '//' are equivalent for non-negative values
-    #   https://github.com/pytorch/pytorch/issues/43874
-    argmin = diff.argmin(0) # [n_tiles_y, n_tiles_x]
-    dy = torch.div(argmin, n_pos, rounding_mode='floor') + search_dist_min # [n_tiles_y, n_tiles_x]
-    dx = argmin % n_pos + search_dist_min # [n_tiles_y, n_tiles_x]
+    min_idx = torch.argmin(diff, -1)
+    dy = min_idx // (2*search_dist+1) - search_dist
+    dx = min_idx %  (2*search_dist+1) - search_dist
 
     # save the current alignment
     alignment = torch.stack([dx, dy], 0) # [2, n_tiles_y, n_tiles_x]
-    
+
     # combine the current alignment with the previous alignment
     alignment += prev_alignment
-    
-    return alignment # [2, n_tiles_y, n_tiles_x]
+
+    return alignment
 
 
 @torch.jit.script
@@ -228,12 +136,12 @@ def warp_images(images: Tensor,
     return images_warped
 
 
-# @torch.jit.script
 def align_and_merge(images: Tensor,
                     ref_idx: int = 0,
                     device: torch.device = torch.device('cpu'),
                     min_layer_res: int = 64,
                     tile_size: int = 16,
+                    search_dist: int = 2,
                    ) -> Tensor:
     """
     Align and merge a burst of images. The input and output tensors are assumed to be on CPU device, to reduce GPU memory requirements.
@@ -257,12 +165,8 @@ def align_and_merge(images: Tensor,
         n_layers += 1
         res /= 2
     
-    # set alignment parameters for each layer
-    downscale_factor_list = n_layers*[2]
-    search_region_list = n_layers*[[-2, 2]]
-    tile_shape_list = n_layers*[[tile_size, tile_size]]
-
     # build a pyramid from the reference image
+    downscale_factor_list = n_layers*[2]
     ref_idx = torch.tensor(ref_idx)
     ref_image = images[ref_idx].to(device)
     ref_pyramid = build_pyramid(ref_image, downscale_factor_list)
@@ -279,14 +183,11 @@ def align_and_merge(images: Tensor,
         # start off with default alignment (no shift between images)
         alignment = torch.zeros([2, 1, 1], device=device)
         
-        # iterative improve the alignment in each pyramid layer
+        # iteratively improve the alignment in each pyramid layer
         for layer_idx in torch.flip(torch.arange(len(ref_pyramid)), [0]):
-            alignment = align_layers(ref_pyramid[layer_idx],
-                             comp_pyramid[layer_idx],
-                             tile_shape_list[layer_idx],
-                             search_region_list[layer_idx],
-                             alignment,
-                             downscale_factor_list[min(layer_idx+1, len(ref_pyramid)-1)])
+            downscale_factor = downscale_factor_list[min(layer_idx+1, len(ref_pyramid)-1)]
+            alignment = align_layers(ref_pyramid[layer_idx], comp_pyramid[layer_idx],
+                                     alignment, tile_size, search_dist, downscale_factor)
             
         # scale the alignment to the resolution of the original image
         alignment = upscale_previous_alignment(alignment, downscale_factor_list[0], W, H)
